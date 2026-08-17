7820899c47809984a671ccb2c1bbc010ad3f3cfb
tesi11alex-cell <tesi11alex@gmail.com>
2026-08-17 02:18:54 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBiQKRrPUfnzCcbk-X4Jan_5GdQaPUTtm0",
+  authDomain: "registro-lavoro-alessandro.firebaseapp.com",
+  projectId: "registro-lavoro-alessandro",
+  storageBucket: "registro-lavoro-alessandro.firebasestorage.app",
+  messagingSenderId: "646710568768",
+  appId: "1:646710568768:web:0a0475677e3ec9a17e3ca5"
+};
